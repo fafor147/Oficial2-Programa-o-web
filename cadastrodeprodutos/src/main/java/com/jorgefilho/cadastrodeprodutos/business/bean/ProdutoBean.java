912feb3912b5481176ff84a813d3b121fa37132c
fafor147 <61852156+fafor147@users.noreply.github.com>
2020-06-19 21:44:25 -0300

    Add files via upload

---
+++ cadastrodeprodutos/src/main/java/com/jorgefilho/cadastrodeprodutos/business/bean/ProdutoBean.java
@@ -0,0 +1,31 @@
+package com.jorgefilho.cadastrodeprodutos.business.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean(name = "produto")
+public class ProdutoBean {
+
+    private String nome;
+    private String categoria;
+    private float preco;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
